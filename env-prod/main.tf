c4129fd25cf25f7094373d064f754b54323954e6
sukanya <sukanyasirigiri@gmail.com>
2023-05-11 15:46:42 -0400

    main

--- env-prod/main.tf
+++ env-prod/main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "test.test1"
-  type  = "String"
+  type  = "secureString"
   value = "Hello World"
+  key_id = b5f53787-daec-4270-a332-836e416b8bdb
 }
