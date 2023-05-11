6b63abf23f24985f47926fe6ff6f47e524dc2e99
sukanya <sukanyasirigiri@gmail.com>
2023-05-11 19:10:49 -0400

    para

---
+++ env-dev/main.tfvars
@@ -0,0 +1,7 @@
+parameters = [
+    { name = "dev.frontend.catalogue.url", value = "http://catalogue.devops1722.com:8080/" },
+     { name = "dev.frontend.cart.url", value = "http://cart.devops1722.com:8080/" },
+    
+
+]
+
--- env-prod/main.tf
+++ env-prod/main.tf
@@ -1,6 +1,7 @@
-resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test1"
-  type  = "secureString"
-  value = "Hello World"
+resource "aws_ssm_parameter" "parameters" {
+    count = lenght(var.parameters)
+  name  = var.parameters[count.index].name
+    value = var.parameters[count.index].value
+  type = "string"
   key_id = b5f53787-daec-4270-a332-836e416b8bdb
 }
