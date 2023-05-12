ce60b57ec2e9dd2e20b1f8b1820efbc5de500a2c
sukanya <sukanyasirigiri@gmail.com>
2023-05-12 11:06:03 -0400

    main

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "parameters" {
    count = length(var.parameters)
   name  = var.parameters[count.index].name
     value = var.parameters[count.index].value
-  type = "string"
+  type = "String"
   key_id = "b5f53787-daec-4270-a332-836e416b8bdb"
 }  
 
